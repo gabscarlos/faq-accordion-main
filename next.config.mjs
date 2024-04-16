a0998802b59addb912372ea20d55ef3d5c0601a7
gabscarlos <gabrielcarllos@yahoo.com.br>
2024-04-16 14:17:02 -0300

    config build

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
     basePath: '/faq-accordion-main',
     assetPrefix: '/faq-accordion-main/',
 };
+
+
 
 export default nextConfig;
